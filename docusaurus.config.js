bbdbb9627605514462b999f130579fc35f08d30d
Ziga Bobnar <ziga@bobnar.net>
2020-03-23 18:08:31 +0100

    Clean up the project

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,6 @@ module.exports = {
           label: 'Documenataion',
           position: 'left',
         },
-        /*{to: 'blog', label: 'Blog', position: 'left'},*/
         {
           href: 'https://github.com/d0si/vortex',
           label: 'GitHub/d0si/vortex',
@@ -30,51 +29,6 @@ module.exports = {
     },
     footer: {
       style: 'dark',
-      links: [
-        /*{
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/doc1',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2',
-            },
-          ],
-        },*/
-        /*{
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-          ],
-        },*/
-        /*{
-          title: 'Social',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },*/
-      ],
       copyright: `Copyright © ${new Date().getFullYear()} Delta Zero. Built with Docusaurus.`,
     },
   },
